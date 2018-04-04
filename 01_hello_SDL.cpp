2b79249ac26095fec9653f1169c99a17ab1bd3cb
0xBECEDA <sakharovasonya1@gmail.com>
2018-04-04 22:22:57 +0300

    добавлен доступ к пикселю

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -24,10 +24,9 @@ SDL_Window* gWindow = NULL;
 // поверхность в окне
 SDL_Surface* gScreenSurface = NULL;
 
-//изображение, которое мы загрузим и покажем на экране
-SDL_Surface* gHelloWorld = NULL;
-
 SDL_Renderer* renderer = NULL;
+
+//*p == NULL;
 
 bool init()
 {
@@ -43,9 +42,9 @@ bool init()
     } else {
         // в противном случае создаем окно
 // gWindow (переменная, внутри которой указатель(?) на место, по которому лежит окно в памяти) приравнена к функции SDL_CreateWindow, которая должна создать окно (?)
-        //    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+           gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
         // если указатель равен NULL,
-        SDL_CreateWindowAndRenderer( SCREEN_WIDTH,SCREEN_HEIGHT , 0, &gWindow, &renderer);
+           // SDL_CreateWindowAndRenderer( SCREEN_WIDTH,SCREEN_HEIGHT , 0, &gWindow, &renderer);
         if( gWindow == NULL ) {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
 // все плохо, устанавливаем false
@@ -53,11 +52,46 @@ bool init()
         } else {
             //в противном случае устанавливаем поверхность окна
             // переменная gScreenSurface (в которой указатель на поверхность окна в памяти (?)) приравнена к функции SDL_GetWindowSurface, у котоой в качестве параметра переменная gWindow, где теперь лежит адрес нашено окна (?)
-            //   gScreenSurface = SDL_GetWindowSurface( gWindow );
+               gScreenSurface = SDL_GetWindowSurface( gWindow );
         }
     }
     // возвращаем "успех" (возвращаем куда? куда попадает "уведомление", что все прошло успешно?)
     return success;
+}
+
+
+Uint32 getpixel( SDL_Surface *surface, int x, int y) {
+
+    SDL_LockSurface(gScreenSurface);
+
+    int bpp =  gScreenSurface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8 *p = (Uint8 *)gScreenSurface->pixels + y *  gScreenSurface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16 *)p;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32 *)p;
+        break;
+
+    default:
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+    SDL_UnlockSurface(gScreenSurface);
 }
 
 bool loadMedia()
@@ -65,25 +99,12 @@ bool loadMedia()
     //устанавливаем флаг
     bool success = true;
 //пробуем загрузить рисунок
-    // переменная  gHelloWorld ( в которой лежит указатель на наш рисунок) приравнена к функции SDL_LoadBMP (которая и грузит изображение, как я понимаю). Чтоб все загрузилось, надо указать правильный путь.
-    // gHelloWorld = SDL_LoadBMP("hello_world.bmp");
-
-    // renderer = SDL_CreateRenderer(gWindow, -1, 0);
-// если указатель в  gHelloWorld рaвен нулю,
-      //  if( gHelloWorld == NULL ) {
-      //  printf( "Unable to load image %s! SDL Error: %s\n", "~/Desktop/make/hello_world.bmp", SDL_GetError() );
-      //  success = false;
-      // }
-      //  if ( renderer == NULL) {
-      // printf( "Unable to load render %s! SDL Error: %s\n", "~/Desktop/make/hello_world.bmp", SDL_GetError() );
-
-    // все плохо, устанавливаем false
-    // success = false;
+    getpixel( gScreenSurface, 50,100);
+    // если указатель в  gHelloWorld рaвен нулю,
+    //  if( *p == NULL ) {
+    //       printf( "Что-то не так с пикселем! SDL Error: %s\n", "~/Desktop/make/hello_world.bmp", SDL_GetError() );
+    //      success = false;
     //  }
-    // в противном случае возвращаем "успех" (опять же, куда? )
- SDL_RenderDrawPoint (renderer, 400, 300);
-  SDL_RenderPresent (renderer);
-
     return success;
 }
 
@@ -91,8 +112,8 @@ void close()
 {
 //зачистка изображение -
     // берем функцию  SDL_FreeSurface с параметром gHelloWorld (зачем?) и говорим, что gHelloWorld снова равен NULL
-    // SDL_FreeSurface( gHelloWorld );
-    // gHelloWorld = NULL;
+    // SDL_FreeSurface(pixel);
+    // pixel  = NULL;
 
     // SDL_RenderClear( renderer );
     // renderer = NULL;
@@ -119,13 +140,9 @@ int main( int argc, char* args[] )
             printf( "Failed to load media!\n" );
         } else {
             //добавляем изображение. Что тут с аргументами пока что не понимаю.
-            //            SDL_BlitSurface( gHelloWorld , NULL, gScreenSurface, NULL );
+            // SDL_BlitSurface( gHelloWorld , NULL, gScreenSurface, NULL );
             //обновляем изображение
-            //    SDL_UpdateWindowSurface( gWindow );
-            SDL_RenderDrawPoint (renderer, 400, 300);
-
-                    SDL_RenderPresent (renderer);
-
+             SDL_UpdateWindowSurface( gWindow );
 
             //ждем пару секунд
             SDL_Delay( 5000 );
